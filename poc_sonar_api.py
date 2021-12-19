bda3168c3e2dbed4a407dc698a8486598d73631f
Samatha2627 <88172701+Samatha2627@users.noreply.github.com>
2021-12-19 17:20:45 -0500

    Create poc_sonar_api.py

---
+++ poc_sonar_api.py
@@ -0,0 +1,8 @@
+import requests
+from requests.auth import HTTPBasicAuth
+token = 'admin'
+PARAM = {'component': 'mvn-poc', 'metricKeys': 'code_smells,bugs,vulnerabilities,ncloc,coverage'}
+test_url = 'http://34.125.196.119:9000/api/measures/component'
+test_response = requests.get(test_url,auth=HTTPBasicAuth(username=token, password="admin"), verify=False,params=PARAM)
+test_json = test_response.json()
+print(test_json)
